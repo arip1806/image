c8ea2cd06ca60a90d01321318f44061c2787c254
arip1806 <35683377+arip1806@users.noreply.github.com>
2024-11-20 17:14:11 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -8,13 +8,13 @@ def rgb_fft(image):
         fft_images_log.append(np.log(abs(rgb_fft)))
     
     return fft_images, fft_images_log
+    
 def apply_mask(input_image, mask): 
     _, mask_thresh = cv2.threshold(mask, 120, 255, cv2.THRESH_BINARY)
     mask_bool = mask_thresh.astype('bool')
     input_image[mask_bool] = 1
     
     return input_image 
-
 
 def apply_mask_all(list_images, list_mask): 
     final_result = []
@@ -23,6 +23,7 @@ def apply_mask_all(list_images, list_mask):
         result = apply_mask(i,mask)
         final_result.append(result)
     return final_result
+    
 def create_canvas_draw_instance(background_image, key, height, width): 
 
     canvas_result = st_canvas(
